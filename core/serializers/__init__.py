f538ecf15abd00d68544ea205ef8359bd89b0035
PedroIFC <ps404412@gmail.com>
2025-03-28 09:16:54 -0300

    Dois serializadores para Livro

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -3,4 +3,4 @@ from .user import UserSerializer
 from .categoria import CategoriaSerializer
 from .editora import EditoraSerializer
 from .autor import AutorSerializer
-from .livro import LivroSerializer
+from .livro import LivroSerializer, LivroListRetrieveSerializer
