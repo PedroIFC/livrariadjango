4c919344b200168dd98ce6f2138c188534b17658
PedroIFC <ps404412@gmail.com>
2025-04-01 16:48:24 -0300

    Terminando aula 12

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 from core.views.livro import LivroViewSet
 from drf_spectacular.views import (
     SpectacularAPIView,
@@ -9,7 +11,7 @@ from drf_spectacular.views import (
 from rest_framework.routers import DefaultRouter
 
 from core.views import AutorViewSet, UserViewSet, CategoriaViewSet, EditoraViewSet
-
+from uploader.router import router as uploader_router
 
 router = DefaultRouter()
 router.register(r"autores", AutorViewSet)
@@ -22,6 +24,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # OpenAPI 3
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    
+    path("api/media/", include(uploader_router.urls)),
     path(
         'api/swagger/',
         SpectacularSwaggerView.as_view(url_name='schema'),
@@ -35,3 +39,4 @@ urlpatterns = [
     # API
     path('api/', include(router.urls)),
 ]
+urlpatterns += static(settings.MEDIA_ENDPOINT, document_root=settings.MEDIA_ROOT)
